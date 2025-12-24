27c365bde963909d41034edcdb07c87dcfd9d07b
Fattah230805 <muhammadfattah23@gmail.com>
2025-12-24 23:19:46 +0700

    commit 1

---
+++ MLProject/modelling.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error, r2_score
+
+df = pd.read_csv("data/preprocess.csv")
+
+X = df.drop("House_Price", axis=1)
+y = df["House_Price"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    rmse = root_mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.sklearn.log_model(model, "model")
+
+print("Training selesai")
